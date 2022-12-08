d1d7c0ca1b0702ed657468dcc7e5b9dfbd73e1dc
Joy Muthoni Malanga <joyalphamuthoni@gmail.com>
2022-12-08 09:52:41 +0300

    added project id to table

--- db/migrate/20221207081710_create_reviews.rb
+++ db/migrate/20221207081710_create_reviews.rb
@@ -3,7 +3,7 @@ class CreateReviews < ActiveRecord::Migration[7.0]
     create_table :reviews do |t|
       t.string :name
       t.text :description
-      t.references :project, null: false, foreign_key: true
+      t.integer :project_id
 
       t.timestamps
     end
