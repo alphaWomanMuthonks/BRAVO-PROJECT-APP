3cce5a968def8d4fb2d983e0362cca42e0c51555
Wanambuko Ngoya Daniel <danielngoys@outlook.com>
2022-12-08 13:33:26 +0300

    modified projects db with postgresql

--- db/migrate/20221207081324_create_projects.rb
+++ db/migrate/20221207081324_create_projects.rb
@@ -3,7 +3,7 @@ class CreateProjects < ActiveRecord::Migration[7.0]
     create_table :projects do |t|
       t.string :name
       t.text :description
-      t.integer :user_id
+      t.integer :user_mask
 
       t.timestamps
     end
