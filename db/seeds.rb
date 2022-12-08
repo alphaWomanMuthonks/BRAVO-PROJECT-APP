c0a2b0ed423e0ee6e3c99058a33b375a4b6c2eff
Joy Muthoni Malanga <joyalphamuthoni@gmail.com>
2022-12-08 10:56:44 +0300

    made changes on seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,18 +6,53 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-10.times do
-  Project.create(
-    name: Faker::App.name,
-    description: Faker::Hipster.sentence,
-    user_id: users.sample.id
+puts"seeding data"
+
+puts"creating fake  users"
+30.times do
+  User.create(
+    email: Faker::Internet.email,
+    username: Faker::Internet.username,
+    password: "password"
   )
 end
 
-50.times do
-  Review.create(
-    name: Faker::App.name,
-    description: Faker::Hipster.sentence,
-    project_id: projects.sample.id
-  )
-end
+puts "creating projects"
+
+Project.create(
+  name: "Project 1",
+  description: "This is the first project",
+  user_id: 1
+)
+
+Project.create(
+  name: "Project 2",
+  description: "This is the second project",
+  user_id: 1
+)
+
+Project.create(
+  name: "Project 3",
+  description: "This is the third project",
+  user_id: 1
+)
+
+puts "creating reviews"
+
+Review.create(
+  name: "Review 1",
+  description: "This is the first review",
+  project_id: 1
+)
+
+Review.create(
+  name: "Review 2",
+  description: "This is the second review",
+  project_id: 1
+)
+
+Review.create(
+  name: "Review 3",
+  description: "This is the third review",
+  project_id: 1
+)
