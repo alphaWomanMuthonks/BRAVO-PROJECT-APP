70a0cfe9cd5b45a3775039c4bc425b41fc3c1dae
Joy Muthoni Malanga <joyalphamuthoni@gmail.com>
2022-12-08 09:30:15 +0300

    modified application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,4 @@
 class ApplicationController < ActionController::API
-    protect_from_forgery with: :null_session
-    before_action :configure_permitted_parameters, if: :devise_controller?
-    before_action :authenticate_user!
-   
+  include ActionController::Cookies
 
-    before_action :configure_permitted_parameters, if: :devise_controller?
-
-    protected
-
-    def configure_permitted_parameters
-        devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email, :password, :password_confirmation])
-    end
 end
